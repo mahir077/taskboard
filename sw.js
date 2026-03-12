7498fa2421fdb86a5d99fc07c44cb73ff74b08bc
Mahir Sadman <sadmanislam856@gmail.com>
2026-03-12 06:01:51 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mahraat-taskboard-v1';
+const CACHE = 'mahraat-taskboard-v2';
 const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
